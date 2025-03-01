9fbeb8a9bf13acfb806bf87d725b33a3938fea3d
Ressil-Distro <zafra.ressil@gmail.com>
2025-03-01 00:02:34 -0600

    about

--- about.php
+++ about.php
@@ -1,19 +1,12 @@
-<?php
-
-phpinfo();
-
-echo '<br> This is a test. Don\'t worry!';
-
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage :: Sample</title>
+    <title>About</title>
 </head>
 <body>
 
-    <h1>Testing Homepage!</h1>
+    <h1>Testing About</h1>
     
 </body>
 </html>
