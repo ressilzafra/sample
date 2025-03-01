81c95cc097481a23b0d128a38b27e969b814932e
Ressil-Distro <zafra.ressil@gmail.com>
2025-02-28 23:55:51 -0600

    added html

--- index.php
+++ index.php
@@ -5,3 +5,13 @@ phpinfo();
 echo '<br> This is a test. Don\'t worry!';
 
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
