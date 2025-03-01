852bd5f2ba2635101f646f34713c74706f4887e1
Ressil <ressil.zafra@gmail.com>
2025-03-01 00:01:03 -0600

    change homepage title

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 phpinfo();
@@ -5,11 +6,17 @@ phpinfo();
 echo '<br> This is a test. Don\'t worry!';
 
 ?>
+=======
+>>>>>>> Stashed changes
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <title>Document</title>
+=======
+    <title>Homepage!</title>
+>>>>>>> Stashed changes
 </head>
 <body>
     
